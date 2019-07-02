cb321e90578c13174369d4146527113cb73dc214
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-07-01 19:14:34 -0500

    configured tensorboard for visualization

--- train.py
+++ train.py
@@ -2,55 +2,49 @@
 Project: ChordNet
 Author: Gabriel Abrantes
 Email: gabrantes99@gmail.com
-Date: 6/30/2019
+Date: 7/1/2019
 Title: train.py
 Description: Trains the neural network.
 """
 
+import time
+import os
+
 from model.chordnet import ChordNet
 from model.data_generator import read_data
-from utils.utils import plot
+from model.train_val_tensorboard import TrainValTensorBoard
 
-from keras.optimizers import Adam
 from keras.utils.vis_utils import plot_model
-from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.callbacks import ModelCheckpoint
 
 EPOCHS = 100
 BATCH_SIZE = 16
 
 def train():
     model = ChordNet.build()
-
     model.summary()
-    plot_model(model, to_file='chordnet.png', show_shapes=True, show_layer_names=True)
 
-    losses = {
-        "soprano": "sparse_categorical_crossentropy",
-        "alto": "sparse_categorical_crossentropy",
-        "tenor": "sparse_categorical_crossentropy",
-        "bass": "sparse_categorical_crossentropy"
-    }
-    loss_weights = {
-        "soprano": 1.0,
-        "alto": 1.0,
-        "tenor": 1.0,
-        "bass": 1.0
-    }
-
-    model.compile(
-        optimizer=Adam(), 
-        loss=losses, 
-        loss_weights=loss_weights,
-        metrics=['accuracy', 'sparse_categorical_accuracy']
-        )    
+    cur_time = time.localtime()
+    log_dir = "./logs/{}.{}.{}{}".format(
+            cur_time[1],
+            cur_time[2],
+            cur_time[3],
+            cur_time[4]
+        )
+    os.makedirs(log_dir)
+    plot_model(model, to_file=log_dir+'/ChordNet.png', show_shapes=True, show_layer_names=True)  
 
     checkpoint = ModelCheckpoint(
         "model.best.hdf5",
         monitor='val_loss',
         save_best_only=True
         )
-    csv = CSVLogger('log.csv')
-    callbacks_list = [checkpoint]
+    
+    tensorboard = TrainValTensorBoard(
+        log_dir=log_dir,
+        write_graph=True
+        )
+    callbacks_list = [checkpoint, tensorboard]
 
     train_X, train_S, train_A, train_T, train_B = read_data("./data/train.txt")
     val_X, val_S, val_A, val_T, val_B = read_data("./data/val.txt")
@@ -65,11 +59,8 @@ def train():
         epochs=EPOCHS,
         batch_size=BATCH_SIZE,
         callbacks=callbacks_list,
-        verbose=1
+        verbose=2
     )
-
-    # Plot training & validation accuracy values
-    plot(H)
 
 if __name__ == "__main__":
     train()
