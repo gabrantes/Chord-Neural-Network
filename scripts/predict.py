224776b69285b51d8234df7c2771435404d87a0b
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-07-15 19:46:47 -0500

    Show ground truth chords

--- scripts/predict.py
+++ scripts/predict.py
@@ -24,6 +24,7 @@ def predict(input_file: str, weights: str):
     """
     # load model
     model = ChordNet.build()
+    print("Loading weights from {}".format(weights))
     model.load_weights(weights)
 
     # get inputs
@@ -32,12 +33,17 @@ def predict(input_file: str, weights: str):
     f.close()
 
     batch_size = len(lines)
-    inputs = np.zeros((batch_size, 41), dtype=np.int16)
+    inputs = np.zeros((batch_size, 45), dtype=np.int16)
     for i in range(batch_size):
-        inputs[i, :] = [int(el) for el in lines[i].split()[:41]]    
+        inputs[i, :] = [int(el) for el in lines[i].split()]  
+
+    feat_inputs = np.zeros((batch_size, 29))  
+    feat_inputs[:, 13] = inputs[:, 13]
+    feat_inputs[:, 13:17] = inputs[:, 25:29]
+    feat_inputs[:, 17:29] = inputs[:, 29:41]
 
     # predict
-    preds = model.predict(inputs, batch_size=batch_size)
+    preds = model.predict(feat_inputs, batch_size=batch_size)
 
     # process output predictions
     outs = np.zeros((batch_size, 4, 21))
@@ -46,6 +52,7 @@ def predict(input_file: str, weights: str):
     next_chords_num = np.argmax(outs, axis=2)  
 
     cur_chords_num = inputs[:, 25:29]
+    gt_chords_num = inputs[:, 41:]
 
     key_num = inputs[:, :12]
     key = np.zeros((batch_size, 2), dtype=np.int8)
@@ -54,20 +61,30 @@ def predict(input_file: str, weights: str):
 
     next_chords =  [None] * batch_size
     cur_chords = [None] * batch_size
+    gt_chords = [None] * batch_size
     
     # unscale notes, convert back to string representations
     satb = Satb()
     for i in range(batch_size):
-        next_chords_num[i, :] = satb.unscale(next_chords_num[i, :].tolist())
-        cur_chords_num[i, :] = satb.unscale(cur_chords_num[i, :].tolist())              
+        try:
+            next_chords_num[i, :] = satb.unscale(next_chords_num[i, :].tolist())
+            next_chords[i] = [num_to_note_key(int(el), key[i, 0], key[i, 1]) for el in next_chords_num[i, :].tolist()]
+        except:
+            next_chords[i] = "INVALID"
+        cur_chords_num[i, :] = satb.unscale(cur_chords_num[i, :].tolist())  
+        gt_chords_num[i, :] = satb.unscale(gt_chords_num[i, :].tolist())            
         
-        next_chords[i] = [num_to_note_key(int(el), key[i, 0], key[i, 1]) for el in next_chords_num[i, :].tolist()]
+        
         cur_chords[i] = [num_to_note_key(int(el), key[i, 0], key[i, 1]) for el in cur_chords_num[i, :].tolist()]
+        gt_chords[i] = [num_to_note_key(int(el), key[i, 0], key[i, 1]) for el in gt_chords_num[i, :].tolist()]
 
     # print predictions
     print("\n")
     for i in range(batch_size):
-        out_str = "{}\t-->\t{}".format(cur_chords[i], next_chords[i])
+        if next_chords[i] == gt_chords[i]:
+            out_str = "{}\t-->\t{}\n".format(cur_chords[i], next_chords[i])
+        else:
+            out_str = "{}\t-->\t{}\tCorrect: {}\n".format(cur_chords[i], next_chords[i], gt_chords[i])
         print(out_str)
 
     return next_chords
@@ -75,8 +92,8 @@ def predict(input_file: str, weights: str):
 if __name__ == "__main__":
     parser  = argparse.ArgumentParser(description='Generate predictions from trained model.')
     parser.add_argument("--init",
-        help="Filepath to model weights. DEFAULT: model.best.hdf5",
-        default='./model/model.best.hdf5')
+        help="Filepath to model weights. DEFAULT: model.feat.hdf5",
+        default='./model/model.feat.hdf5')
     parser.add_argument("--input", 
         help="Filepath to processed dataset. DEFAULT: ./data/test.txt",
         default="./data/test.txt")
